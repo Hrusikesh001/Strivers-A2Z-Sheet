4cc4132e940f3523701ced35fde1bf30ba826f6c
Hrusikesh123 <hs2068672@gmail.com>
2026-07-31 19:15:51 +0530

    Add while_loop_04 implementation to print a message in a loop

---
+++ Basics/while_loop_04.java
@@ -0,0 +1,9 @@
+public class while_loop_04 {
+    public static void main(String[] args){
+        int i = 1;
+        while(i <= 10) {
+            System.out.println("Hrusikesh");
+            i++;
+        }
+    }
+}
