6a1624bdf36c81f656ed43ed4f3aa1645ba21de0
Hrusikesh123 <hs2068672@gmail.com>
2026-07-31 19:22:25 +0530

    Add do_while_05 implementation to print a message in a loop

---
+++ Basics/do_while_05.java
@@ -0,0 +1,9 @@
+public class do_while_05 {
+    public static void main(String[] args) {
+        int i = 2;
+        do{
+            System.out.println("Hrusikesh");
+            i++;
+        }while(i <= 10);
+    }
+}
