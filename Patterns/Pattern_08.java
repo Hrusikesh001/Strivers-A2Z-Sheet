a2960f8076664352de340fa50a1df2fc36c7086e
Hrusikesh123 <hs2068672@gmail.com>
2026-08-03 13:40:20 +0530

    Add initial implementation of Pattern_08 to print an inverted pyramid pattern of asterisks

---
+++ Patterns/Pattern_08.java
@@ -0,0 +1,26 @@
+// *******
+//  ***** 
+//   ***  
+//    *   
+
+public class Pattern_08 {
+    public static void main(String[] args) {
+        int n = 4;
+        for(int i=0; i<n; i++) {{
+            //space
+            for(int j=0; j<i; j++) {
+                System.out.print(" ");
+            }
+            //star
+            for(int k=0; k<2*n-(2*i+1); k++) {
+                System.out.print("*");
+            }
+            //space
+            for(int j=0; j<i; j++) {
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
+        }
+    }
+}
