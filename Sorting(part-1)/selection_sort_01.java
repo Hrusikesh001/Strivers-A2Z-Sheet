84416e3fd7473e8c2b5fcf3038f00de773934c29
Hrusikesh123 <hs2068672@gmail.com>
2026-08-13 00:05:28 +0530

    Add implementation of selection_sort_01 to perform selection sort on an array

---
+++ Sorting(part-1)/selection_sort_01.java
@@ -0,0 +1,26 @@
+public class selection_sort_01 {
+    static void selectionSort(int[] arr, int n) {
+        for (int i = 0; i <= n - 2; i++) {
+            int mini = i;
+            for (int j = i; j <= n-1; j++) {
+                if (arr[j] < arr[mini]) {
+                    mini = j;
+                }
+            }
+            // Swap
+            int temp = arr[mini];
+            arr[mini] = arr[i];
+            arr[i] = temp;
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = { 13, 46, 24, 52, 20, 9 };
+        int n = arr.length;
+
+        selectionSort(arr, n);
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
