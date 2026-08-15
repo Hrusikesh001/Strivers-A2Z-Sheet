642feb9ada608b1a08b198d2b7d84bf21187f755
Hrusikesh123 <hs2068672@gmail.com>
2026-08-15 16:14:28 +0530

    Add implementation of quick sort algorithm to sort an array

---
+++ Sorting(part-1)/quick_sort_07.java
@@ -0,0 +1,36 @@
+public class quick_sort_07 {
+    public static void quickSort(int[] arr, int low, int high) {
+        if(low >= high) {
+            return;
+        }
+        int pivot = arr[low];
+        int i = low;
+        int j = high;
+        while(i < j) {
+            while(arr[i] <= pivot && i <= high) {
+                i++;
+            }
+            while(arr[j] > pivot && j >= low) {
+                j--;
+            }
+            if(i < j) {
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+
+        }
+        int temp = arr[low];
+        arr[low] = arr[j];
+        arr[j] = temp;
+        quickSort(arr, low, j - 1);
+        quickSort(arr, j + 1, high);
+    }
+    public static void main(String[] args) {
+        int[] arr = {4, 6, 2, 5, 7, 9, 1, 3};
+        quickSort(arr, 0, arr.length - 1);
+        for(int i : arr) {
+            System.out.print(i + " ");
+        }
+    }
+}
