fe0c384ae1758b5a896ff5366f65a281c18d1bb2
Hrusikesh123 <hs2068672@gmail.com>
2026-08-19 00:03:51 +0530

    Add implementation to rotate an array to the left by k places

---
+++ Array(Easy)/rotate_array_by_k_place_06.java
@@ -0,0 +1,28 @@
+//rotate to the left by k place
+public class rotate_array_by_k_place_06 {
+    public static void rotateArray(int[] arr, int k) {
+        int n = arr.length;
+        k = k % n;
+        int[] temp = new int[k];
+        for(int i=0; i<k; i++) {
+            temp[i] = arr[i];
+        }
+        for(int i=k; i<n; i++){
+            arr[i-k] = arr[i];
+        }
+        for(int i=n-k; i<n; i++){
+            arr[i] = temp[i-(n-k)];
+        }
+    }
+    public static void main(String[] args) {
+        int[] arr = { 1, 2, 3, 4, 5, 6, 7 };
+
+        int k = 3;
+
+        rotateArray(arr, k);
+
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+    }
+}
