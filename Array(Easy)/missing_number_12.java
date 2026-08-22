e7a793c4689c3f1b78528f0f2361a39a7a860ed5
Hrusikesh123 <hs2068672@gmail.com>
2026-08-23 00:44:06 +0530

    Refactor missing number implementation to use sum formula for improved efficiency

--- Array(Easy)/missing_number_12.java
+++ Array(Easy)/missing_number_12.java
@@ -26,24 +26,42 @@
 
 
 //Better Approach
+// public class missing_number_12 {
+//         public static int missingNumber(int[] nums) {
+//             int n = nums.length;
+//             boolean[] hash = new boolean[n + 1];
+//             for (int i = 0; i < n; i++) {
+//                 hash[nums[i]] = true;
+//             }
+
+//             for (int i = 0; i <= n; i++) {
+//                 if (!hash[i]) {
+//                     return i;
+//                 }
+//             }
+//             return -1;
+//         }
+
+//         public static void main(String[] args) {
+//             int[] nums = { 3, 0,2,4, 1 };
+//             System.out.println(missingNumber(nums));
+//         }
+// }
+
+
+// Optimal Approach — Sum Formula
 public class missing_number_12 {
         public static int missingNumber(int[] nums) {
             int n = nums.length;
-            boolean[] hash = new boolean[n + 1];
+            int expectedSum = n * (n + 1) / 2;
+            int actualSum = 0;
             for (int i = 0; i < n; i++) {
-                hash[nums[i]] = true;
+                actualSum += nums[i];
             }
-
-            for (int i = 0; i <= n; i++) {
-                if (!hash[i]) {
-                    return i;
-                }
-            }
-            return -1;
+            return expectedSum - actualSum;
         }
-
         public static void main(String[] args) {
-            int[] nums = { 3, 0,2,4, 1 };
+            int[] nums = { 3,2, 0, 1 };
             System.out.println(missingNumber(nums));
         }
 }
