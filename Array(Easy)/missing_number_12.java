5144886ee345bb76d55763a87896ec380c844c0d
Hrusikesh123 <hs2068672@gmail.com>
2026-08-23 00:13:38 +0530

    Refactor missing number implementation to use a boolean array for improved efficiency

--- Array(Easy)/missing_number_12.java
+++ Array(Easy)/missing_number_12.java
@@ -1,16 +1,41 @@
-//Bruteforce approach
+// //Bruteforce approach
+// public class missing_number_12 {
+//         public static int missingNumber(int[] nums) {
+//             int n = nums.length;
+//             for (int i = 0; i <= n; i++) {
+//                 boolean found = false;
+//                 for (int j = 0; j < n; j++) {
+//                     if (nums[j] == i) {
+//                         found = true;
+//                         break;
+//                     }
+//                 }
+//                 if (!found) {
+//                     return i;
+//                 }
+//             }
+//             return -1;
+//         }
+
+//         public static void main(String[] args) {
+//             int[] nums = {0, 1, 2, 4, 5 };
+//             System.out.println(missingNumber(nums));
+//         }
+
+// }
+
+
+//Better Approach
 public class missing_number_12 {
         public static int missingNumber(int[] nums) {
             int n = nums.length;
+            boolean[] hash = new boolean[n + 1];
+            for (int i = 0; i < n; i++) {
+                hash[nums[i]] = true;
+            }
+
             for (int i = 0; i <= n; i++) {
-                boolean found = false;
-                for (int j = 0; j < n; j++) {
-                    if (nums[j] == i) {
-                        found = true;
-                        break;
-                    }
-                }
-                if (!found) {
+                if (!hash[i]) {
                     return i;
                 }
             }
@@ -18,8 +43,7 @@ public class missing_number_12 {
         }
 
         public static void main(String[] args) {
-            int[] nums = {0, 1, 2, 4, 5 };
+            int[] nums = { 3, 0,2,4, 1 };
             System.out.println(missingNumber(nums));
         }
-    
 }
