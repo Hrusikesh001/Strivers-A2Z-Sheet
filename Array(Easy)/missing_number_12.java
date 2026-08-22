c2e9f88aa029f90149f17c8fe4224ee63f3ae57d
Hrusikesh123 <hs2068672@gmail.com>
2026-08-22 23:57:37 +0530

    Add brute force implementation to find missing number in an array

---
+++ Array(Easy)/missing_number_12.java
@@ -0,0 +1,25 @@
+//Bruteforce approach
+public class missing_number_12 {
+        public static int missingNumber(int[] nums) {
+            int n = nums.length;
+            for (int i = 0; i <= n; i++) {
+                boolean found = false;
+                for (int j = 0; j < n; j++) {
+                    if (nums[j] == i) {
+                        found = true;
+                        break;
+                    }
+                }
+                if (!found) {
+                    return i;
+                }
+            }
+            return -1;
+        }
+
+        public static void main(String[] args) {
+            int[] nums = {0, 1, 2, 4, 5 };
+            System.out.println(missingNumber(nums));
+        }
+    
+}
