b7675a9e16f21c13ab65971b5993703bbd9ac0b6
Hrusikesh123 <hs2068672@gmail.com>
2026-08-24 15:55:59 +0530

    Refactor single number implementation to use XOR for optimal performance

--- Array(Easy)/single_one_14.java
+++ Array(Easy)/single_one_14.java
@@ -1,22 +1,39 @@
 
 //Bruteforce approach
+// public class single_one_14 {
+//         public static int findSingle(int[] nums) {
+//             for (int i = 0; i < nums.length; i++) {
+//                 int count = 0;
+//                 for (int j = 0; j < nums.length; j++) {
+//                     if (nums[i] == nums[j]) {
+//                         count++;
+//                     }
+//                 }
+//                 if (count == 1) {
+//                     return nums[i];
+//                 }
+//             }
+//             return -1;
+//         }
+//         public static void main(String[] args) {
+//             int[] nums = { 4, 1, 2, 1, 2 };
+//             System.out.println(findSingle(nums));
+//         }
+// }
+
+
+//optimal approach
 public class single_one_14 {
-        public static int findSingle(int[] nums) {
-            for (int i = 0; i < nums.length; i++) {
-                int count = 0;
-                for (int j = 0; j < nums.length; j++) {
-                    if (nums[i] == nums[j]) {
-                        count++;
-                    }
-                }
-                if (count == 1) {
-                    return nums[i];
-                }
-            }
-            return -1;
+    public static int singleNumber(int[] nums) {
+        int xor = 0;
+        for (int i = 0; i < nums.length; i++) {
+            xor = xor ^ nums[i];
         }
-        public static void main(String[] args) {
-            int[] nums = { 4, 1, 2, 1, 2 };
-            System.out.println(findSingle(nums));
-        }
+        return xor;
+    }
+    
+    public static void main(String[] args) {
+        int[] nums = { 4, 1, 2, 1, 2 };
+        System.out.println(singleNumber(nums));
+    }
 }
