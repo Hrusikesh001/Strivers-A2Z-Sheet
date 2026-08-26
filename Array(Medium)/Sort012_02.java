a3ad78e90aa75a4b0ec63d8be3e270732eca9ce7
Hrusikesh123 <hs2068672@gmail.com>
2026-08-26 16:04:08 +0530

    Add Sort012 implementation using counting sort for optimal performance

---
+++ Array(Medium)/Sort012_02.java
@@ -0,0 +1,53 @@
+// //Bruteforce approach
+// import java.util.*;
+// public class Sort012_02 {
+//     public static void main(String[] args) {
+//         int[] arr = {2, 0, 2, 1, 1, 0};
+//         Arrays.sort(arr);
+//         for (int num : arr) {
+//             System.out.print(num + " ");
+//         }
+//     }
+// }
+
+
+
+//Better Approach
+public class Sort012_02 {
+        public static void sortArray(int[] arr) {
+            int count0 = 0;
+            int count1 = 0;
+            int count2 = 0;
+            for (int num : arr) {
+                if (num == 0) {
+                    count0++;
+                } else if (num == 1) {
+                    count1++;
+                } else {
+                    count2++;
+                }
+            }
+            int index = 0;
+            while (count0 > 0) {
+                arr[index++] = 0;
+                count0--;
+            }
+            while (count1 > 0) {
+                arr[index++] = 1;
+                count1--;
+            }
+            while (count2 > 0) {
+                arr[index++] = 2;
+                count2--;
+            }
+        }
+
+        public static void main(String[] args) {
+            int[] arr = { 2, 0, 2, 1, 1, 0 };
+            sortArray(arr);
+            for (int num : arr) {
+                System.out.print(num + " ");
+            }
+        }
+}
+
