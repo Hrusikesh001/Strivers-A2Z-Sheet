36f2eb80349843b2be348ab353531a00ac888e84
Hrusikesh123 <hs2068672@gmail.com>
2026-08-28 17:38:01 +0530

    Refactor maxSubarraySum implementation for improved efficiency by reducing nested loops

--- Array(Medium)/max_subarry_sum_04.java
+++ Array(Medium)/max_subarry_sum_04.java
@@ -1,13 +1,35 @@
+// public class max_subarry_sum_04 {
+//     public static int maxSubarraySum(int arr[]) {
+//         int n = arr.length;
+//         int maxSum = Integer.MIN_VALUE;
+//         for(int i = 0; i < n; i++){
+//             for(int j = i; j < n; j++) {
+//                 int sum = 0;
+//                 for(int k = i; k <= j; k++) {
+//                     sum += arr[k];
+//                 }
+//                 maxSum = Math.max(maxSum, sum);
+//             }
+//         }
+//         return maxSum;
+//     }
+//     public static void main(String[] args) {
+//         int arr[] = {-2, -3, 4, -1, -2, 1, 5, -3};
+//         System.out.println(maxSubarraySum(arr));
+//     }
+// }
+
+
+
+//Better Approach
 public class max_subarry_sum_04 {
-    public static int maxSubarraySum(int arr[]) {
+    public static int maxSubarraySum(int[] arr) {
         int n = arr.length;
         int maxSum = Integer.MIN_VALUE;
-        for(int i = 0; i < n; i++){
-            for(int j = i; j < n; j++) {
-                int sum = 0;
-                for(int k = i; k <= j; k++) {
-                    sum += arr[k];
-                }
+        for (int i = 0; i < n; i++) {
+            int sum = 0;
+            for (int j = i; j < n; j++) {
+                sum += arr[j];
                 maxSum = Math.max(maxSum, sum);
             }
         }
@@ -17,4 +39,5 @@ public class max_subarry_sum_04 {
         int arr[] = {-2, -3, 4, -1, -2, 1, 5, -3};
         System.out.println(maxSubarraySum(arr));
     }
+
 }
