faa299e53fdb88bdb0f4d2e66e5eff113714c92d
Hrusikesh123 <hs2068672@gmail.com>
2026-08-28 10:15:44 +0530

    Refactor majorityElement implementation to use HashMap for optimal performance

--- Array(Medium)/majority_element_03.java
+++ Array(Medium)/majority_element_03.java
@@ -1,20 +1,42 @@
+// public class majority_element_03 {
+//     public static int majorityElement(int arr[]) {
+//         int n = arr.length;
+//         for (int i = 0; i < n; i++) {
+//             int count = 0;
+//             for (int j = 0; j < n; j++) {
+//                 if (arr[i] == arr[j]) {
+//                     count++;
+//                 }
+//             }
+//             if (count > n / 2) {
+//                 return arr[i];
+//             }
+//         }
+//         return -1;
+//     }
+
+//     public static void main(String[] args) {
+//         int arr[] = { 2, 2, 3, 3, 1, 2, 2 };
+//         System.out.println(majorityElement(arr));
+//     }
+// }
+
+
+
+//Better Approach
+import java.util.HashMap;
 public class majority_element_03 {
-    public static int majorityElement(int arr[]) {
+    public  static int majorityElement(int arr[]) {
         int n = arr.length;
+        HashMap<Integer, Integer> map = new HashMap<>();
         for (int i = 0; i < n; i++) {
-            int count = 0;
-            for (int j = 0; j < n; j++) {
-                if (arr[i] == arr[j]) {
-                    count++;
-                }
-            }
-            if (count > n / 2) {
+            map.put(arr[i], map.getOrDefault(arr[i], 0) + 1);
+            if (map.get(arr[i]) > n / 2) {
                 return arr[i];
             }
         }
         return -1;
     }
-
     public static void main(String[] args) {
         int arr[] = { 2, 2, 3, 3, 1, 2, 2 };
         System.out.println(majorityElement(arr));
