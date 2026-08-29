61c1f15d4a0a6befa97b2d73dee4edaa81f93239
Hrusikesh123 <hs2068672@gmail.com>
2026-08-29 15:15:30 +0530

    Add implementation for removeOuterParentheses with optimal approach using StringBuilder

---
+++ Strings/removeOuterParentheses_01.java
@@ -0,0 +1,59 @@
+// import java.util.*;
+// public class removeOuterParentheses_01 {
+//     public static String removeOuterParentheses(String s) {
+//         String ans = "";
+//         Stack<Character> st = new Stack<>();
+
+//         for (char ch : s.toCharArray()) {
+//             if (ch == ')') {
+//                 st.pop();
+//             }
+
+//             if (!st.empty()) {
+//                 ans += ch;
+//             }
+
+//             if (ch == '(') {
+//                 st.push(ch);
+//             }
+//         }
+
+//         return ans;
+//     }
+
+//     public static void main(String[] args) {
+//         String s = "()(())(())(()(()))";
+//         System.out.println(removeOuterParentheses(s));
+//     }
+// }
+
+
+
+
+//Optimal Approach
+import java.util.Stack;
+public class removeOuterParentheses_01 {
+    public static String removeOuterParentheses(String s) {
+        StringBuilder ans = new StringBuilder();
+        int count = 0;
+        for (char ch : s.toCharArray()) {
+            if (ch == '(') {
+                if (count == 0) {
+                    ans.append(ch);
+                }
+                count++;
+            }
+            else {
+                count--;
+                if (count >0) {
+                    ans.append(ch);
+                }
+            }
+        }
+        return ans.toString();
+    }
+    public static void main(String[] args) {
+        String s = "()(())(())(()(()))";
+        System.out.println(removeOuterParentheses(s));
+    }
+}
