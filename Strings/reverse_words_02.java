c548a53d75f197c96d50492e62780020d747e43c
Hrusikesh123 <hs2068672@gmail.com>
2026-08-30 00:13:56 +0530

    Refactor reverseWords method to use StringBuilder for optimal word reversal

--- Strings/reverse_words_02.java
+++ Strings/reverse_words_02.java
@@ -1,42 +1,79 @@
-import java.util.Stack;
-public class reverse_words_02 {
-    public static String reverseWords(String s) {
-        Stack<String> stack = new Stack<>();
-        String word = "";
+// import java.util.Stack;
+// public class reverse_words_02 {
+//     public static String reverseWords(String s) {
+//         Stack<String> stack = new Stack<>();
+//         String word = "";
 
-        // Traverse the string
-        for (int i = 0; i < s.length(); i++) {
-            char ch = s.charAt(i);
-            if (ch != ' ') {
-                word += ch;
-            } else {
-                // If a complete word is found
-                if (!word.isEmpty()) {
-                    stack.push(word);
-                    word = "";
+//         // Traverse the string
+//         for (int i = 0; i < s.length(); i++) {
+//             char ch = s.charAt(i);
+//             if (ch != ' ') {
+//                 word += ch;
+//             } else {
+//                 // If a complete word is found
+//                 if (!word.isEmpty()) {
+//                     stack.push(word);
+//                     word = "";
+//                 }
+//             }
+//         }
+
+//         // Push the last word
+//         if (!word.isEmpty()) {
+//             stack.push(word);
+//         }
+
+//         // Build answer by popping words
+//         StringBuilder ans = new StringBuilder();
+//         while (!stack.isEmpty()) {
+//             ans.append(stack.pop());
+//             if (!stack.isEmpty()) {
+//                 ans.append(" ");
+//             }
+//         }
+//         return ans.toString();
+//     }
+
+//     public static void main(String[] args) {
+//         String s = "a good   example";
+//         System.out.println(reverseWords(s));
+//     }
+
+// }
+
+
+
+
+//Optimal Approach
+public class reverse_words_02 {
+        public static String reverseWords(String s) {
+            StringBuilder ans = new StringBuilder();
+            int i = s.length() - 1;
+            while (i >= 0) {
+                // Skip spaces
+                while (i >= 0 && s.charAt(i) == ' ') {
+                    i--;
+                }
+
+                // Find the end of the word
+                int end = i;
+
+                // Move until the beginning of the word
+                while (i >= 0 && s.charAt(i) != ' ') {
+                    i--;
+                }
+
+                // Add the word
+                if (end >= 0) {
+                    ans.append(s.substring(i + 1, end + 1));
+                    ans.append(" ");
                 }
             }
+            return ans.toString().trim();
         }
 
-        // Push the last word
-        if (!word.isEmpty()) {
-            stack.push(word);
+        public static void main(String[] args) {
+            String s = "a good   example";
+            System.out.println(reverseWords(s));
         }
-
-        // Build answer by popping words
-        StringBuilder ans = new StringBuilder();
-        while (!stack.isEmpty()) {
-            ans.append(stack.pop());
-            if (!stack.isEmpty()) {
-                ans.append(" ");
-            }
-        }
-        return ans.toString();
-    }
-
-    public static void main(String[] args) {
-        String s = "a good   example";
-        System.out.println(reverseWords(s));
-    }
-
 }
