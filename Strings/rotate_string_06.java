7d0cb09c871679afcf024bca4c68858f9afc645a
Hrusikesh123 <hs2068672@gmail.com>
2026-08-30 12:32:53 +0530

    Implement rotateString method to check if one string is a rotation of another

---
+++ Strings/rotate_string_06.java
@@ -0,0 +1,19 @@
+public class rotate_string_06 {
+    public static boolean rotateString(String s, String goal) {
+        if(s.length() != goal.length()) {
+            return false;
+        }
+        for(int i =0; i<s.length(); i++) {
+            if(s.equals(goal)) {
+                return true;
+            }
+            s = s.substring(1) + s.charAt(0);
+        }
+        return false;
+    }
+    public static void main(String[] args) {
+        String s = "abcdefg";
+        String goal = "cdefgab";
+        System.out.println(rotateString(s, goal));
+    }
+}
