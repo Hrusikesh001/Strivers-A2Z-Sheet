b625a2f72510d6b15ed06a24b483aac646517506
Hrusikesh123 <hs2068672@gmail.com>
2026-08-30 12:37:09 +0530

    Implement optimized solution for rotateString method to improve performance and readability

--- Strings/rotate_string_06.java
+++ Strings/rotate_string_06.java
@@ -1,15 +1,33 @@
+// public class rotate_string_06 {
+//     public static boolean rotateString(String s, String goal) {
+//         if(s.length() != goal.length()) {
+//             return false;
+//         }
+//         for(int i =0; i<s.length(); i++) {
+//             if(s.equals(goal)) {
+//                 return true;
+//             }
+//             s = s.substring(1) + s.charAt(0);
+//         }
+//         return false;
+//     }
+//     public static void main(String[] args) {
+//         String s = "abcdefg";
+//         String goal = "cdefgab";
+//         System.out.println(rotateString(s, goal));
+//     }
+// }
+
+
+
+// optimized solution
 public class rotate_string_06 {
     public static boolean rotateString(String s, String goal) {
-        if(s.length() != goal.length()) {
+        if (s.length() != goal.length()) {
             return false;
         }
-        for(int i =0; i<s.length(); i++) {
-            if(s.equals(goal)) {
-                return true;
-            }
-            s = s.substring(1) + s.charAt(0);
-        }
-        return false;
+        String combined = s + s;
+        return combined.contains(goal);
     }
     public static void main(String[] args) {
         String s = "abcdefg";
